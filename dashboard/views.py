536c17af6a5543649d69e2a573babdfad9493b20
Louis George <louisg495@gmail.com>
2020-04-12 15:08:03 -0700

    Add report form started

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Report
+from .forms import ReportForm
+
+
+def is_staff(user):
+    if user.groups.filter(name="Manager").exists() | user.groups.filter(name="Technician").exists():
+        return True
+    else:
+        return False
 
 # Dasboard page. Need diff for client/staff?
 @login_required
@@ -10,5 +18,16 @@ def home(request):
 
 # Data Entry page
 @login_required
+@user_passes_test(is_staff)
 def input(request):
-    return render(request, 'dashboard/input.html')
+    if request.method == 'POST':
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data.get('name')
+            messages.success(request, f'Report Submitted for {name}')
+            # return redirect('url name')
+            # redirect to a page which says report successfully created and asks what user wants to do
+    else:
+        form = ReportForm()
+    return render(request, 'dashboard/input.html', {'form': form})
