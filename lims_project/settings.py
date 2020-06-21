7f2e4702874bf130983a6a39ed60329f489b6491
Louis George <louisg495@gmail.com>
2020-06-20 20:23:43 -0700

    changed debug to environ variable

--- lims_project/settings.py
+++ lims_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("LIMS_CONFIG")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['lims-demo.herokuapp.com']
 
